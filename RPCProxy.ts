c4fe38d8ab61072b8f4766e58d6405ba0ab7c897
Jonas Lekevicius <jonas@lekevicius.com>
2023-01-19 20:12:44 +0200

    WebSockets RPC Proxy

--- RPCProxy.ts
+++ RPCProxy.ts
@@ -2,8 +2,11 @@ import { ethers } from './deps.ts'
 import { config } from './deps.ts'
 
 const env = config()
-const rpcUrl = `https://${env['CHAIN_NAME']}.infura.io/v3/${env['INFURA_KEY']}`
-const rpc = new ethers.providers.JsonRpcProvider(rpcUrl, {
+
+const rpcUrl = `wss://${env['CHAIN_NAME']}.infura.io/ws/v3/${env['INFURA_KEY']}`
+const ws = new WebSocket(rpcUrl)
+
+const rpc = new ethers.providers.WebSocketProvider(ws, {
   name: env['CHAIN_NAME'],
   chainId: parseInt(env['CHAIN_ID']),
 })
