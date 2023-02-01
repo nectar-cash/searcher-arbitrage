37cf33d0f6bac3ea091bdf7f5cd5e470a3316d39
Jonas Lekevicius <jonas@lekevicius.com>
2023-02-01 12:58:29 +0200

    Reconnecting webcokets

--- RPCProxy.ts
+++ RPCProxy.ts
@@ -1,14 +1,21 @@
 import { ethers } from './deps.ts'
 import { config } from './deps.ts'
+import SturdyWebSocket from './SturdyWebsocket.ts'
 
 const env = config()
 
 const rpcUrl = `wss://${env['CHAIN_NAME']}.infura.io/ws/v3/${env['INFURA_KEY']}`
-const ws = new WebSocket(rpcUrl)
+const ws = new SturdyWebSocket(rpcUrl)
+// const ws = new WebSocket(rpcUrl)
 
 const rpc = new ethers.providers.WebSocketProvider(ws, {
   name: env['CHAIN_NAME'],
   chainId: parseInt(env['CHAIN_ID']),
 })
+
+setTimeout(() => {
+  ws.reconnect()
+  console.log('reconnecting websockets')
+}, 60 * 60 * 1000)
 
 export default rpc
